ace6cfb8c06951590e6c1e054617800ba0c8ca87
AnshulGaikwad976 <gaikwad.anshul14@gmail.com>
2025-11-27 18:33:34 +0200

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,18 @@
+fn = open('codingal.txt' ,'r')
+fn1 = open('codingalupdated.txt', 'w')
+
+cont = fn.readlines()
+type(cont)
+for i in range(1 ,len(cont)+1):
+    if(i %2 !=0):
+        fn1.write(cont[i - 1])
+
+    else :
+             pass
+
+fn1.close()
+fn1=open('codingalupdated.txt', 'r')
+
+con1 = fn1.read()
+
+print(con1)
